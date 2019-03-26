fe09c55d83b471da21975354b43edd443311ea0c
Prabhvir Saran <prabhvirsaran@gmail.com>
2019-03-25 23:16:19 -0700

    working with expression and attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ['tag1', 'tag2', 'tag3']
+    //imageUrl: 'https://picsum.photos/200'
+  };
+
+  styles = { 
+    fontSize: 10, //10px
+    fontWeight: "bold"
+
+  };
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hellow World</h1>
-        <button>Increment</button>
+        <img src={ this.state.imageUrl } alt="" />
+
+        {/* Styles expects an object */}
+        <span style = { this.styles } className={ this.getBadgeClasses() } >{ this.formatCount() }</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          { this.state.tags.map( tag => <li key= { tag }>{ tag }</li>)}
+        </ul>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0 ? "warning" : "primary");
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
