b2a02511059464ffced27b7811746e77d79a5fc9
Prabhvir Saran <prabhvirsaran@gmail.com>
2019-04-09 22:26:57 -0700

    using props

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+//Stateless functional components
+
+const NavBar = (props) => {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="www.google.ca">
+          <span className="badge badge-pill badge-secondary">
+            {props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+};
+
+// Instead of using a class
+// class NavBar extends Component {
+//   render() {
+//    );
+//   }
+// }
+
+export default NavBar;
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-    tags: []
-    //imageUrl: 'https://picsum.photos/200'
-  };
 
   styles = {
     fontSize: 10, //10px
@@ -17,33 +12,40 @@ class Counter extends Component {
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
-  handleIncrement = (product) => {
-    console.log(product);
-    this.setState({ value: this.state.value + 1 });
-  };
-
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
     return (
       <ul>
-        {this.state.tags.map(tag => ( <li key={tag}>{tag}</li>))}
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
       </ul>
     );
   }
 
   render() {
-
     let product = 1;
-    console.log('props', this.props);
 
     return (
       <div>
         {/* <img src={ this.state.imageUrl } alt="" /> */}
         {/* Styles expects an object */}
+        {this.props.children}
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button onClick={ () => this.handleIncrement(product) } className="btn btn-secondary btn-sm" > Increment </button>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm">
+          {" "}
+          Increment{" "}
+        </button>
+
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btm-sm m-2">
+          Delete
+        </button>
 
         {/* {this.state.tags.length === 0 && "Please create a new tag"} */}
 
@@ -54,12 +56,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
